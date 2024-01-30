58de8df1a2398b7f2fd9e74f2d87ac7fb5389c8b
PgmJun <chltmdwns96@gmail.com>
2024-01-30 18:54:46 +0900

    feat: 강아지, 고양이 얼굴 인식 API 분리 #8

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from fastapi import FastAPI, UploadFile, File, HTTPException
 from typing import List
 
-import numpy as np
 import cv2
+import numpy as np
+from fastapi import FastAPI, UploadFile, File, HTTPException
 from starlette.status import HTTP_413_REQUEST_ENTITY_TOO_LARGE
 
 app = FastAPI()
@@ -11,7 +11,7 @@ dog_cascade = cv2.CascadeClassifier('model/dog_face.xml')
 cat_cascade = cv2.CascadeClassifier('model/haarcascade_frontalcatface_extended.xml')
 
 
-@app.post("/detectDogFace")
+@app.post("/detect/dog")
 async def detect_dog_face_endpoint(files: List[UploadFile] = File(...)):
     results = []
 
@@ -19,7 +19,20 @@ async def detect_dog_face_endpoint(files: List[UploadFile] = File(...)):
         await validate_file_size(file)
         await file.seek(0)
         contents = await file.read()
-        results.append(await detect_dog_or_cat_face(contents))
+        results.append(await detect_dog_face(contents))
+
+    return {"results": results}
+
+
+@app.post("/detect/cat")
+async def detect_cat_face_endpoint(files: List[UploadFile] = File(...)):
+    results = []
+
+    for file in files:
+        await validate_file_size(file)
+        await file.seek(0)
+        contents = await file.read()
+        results.append(await detect_cat_face(contents))
 
     return {"results": results}
 
@@ -34,7 +47,7 @@ async def validate_file_size(file):
             )
 
 
-async def detect_dog_or_cat_face(contents):
+async def detect_dog_face(contents):
     # 이미지를 NumPy 배열로 변환
     img = np.asarray(bytearray(contents), dtype="uint8")
     img = cv2.imdecode(img, cv2.IMREAD_COLOR)
@@ -42,10 +55,23 @@ async def detect_dog_or_cat_face(contents):
     # 그레이 스케일 변환
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # 강아지/고양이 얼굴 감지
+    # 강아지 얼굴 감지
     dogs = dog_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
+
+    # 감지된 얼굴이 있는지 여부를 반환
+    return len(dogs) > 0
+
+
+async def detect_cat_face(contents):
+    # 이미지를 NumPy 배열로 변환
+    img = np.asarray(bytearray(contents), dtype="uint8")
+    img = cv2.imdecode(img, cv2.IMREAD_COLOR)
+
+    # 그레이 스케일 변환
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # 고양이 얼굴 감지
     cats = cat_cascade.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5)
 
     # 감지된 얼굴이 있는지 여부를 반환
-
-    return (len(dogs) + len(cats)) > 0
+    return len(cats) > 0
